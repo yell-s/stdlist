5a5327e23703f709420eed0dc24d2f876fc671f5
Yell <yellzindtz@gmail.com>
2025-08-18 08:38:16 -0500

    Project created

---
+++ src/main/java/com/example/StdList/StdListApplication.java
@@ -0,0 +1,13 @@
+package com.example.StdList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StdListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StdListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/StdList/StdListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StdList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StdListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
